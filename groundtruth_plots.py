85dc94c279e4c6f29e893fe7805338a0e8909c52
hockeylilly <87642105+hockeylilly@users.noreply.github.com>
2024-03-06 13:06:41 +0100

    modifiziert, haben jetzt 2 pngs die erstellt werden und einige plots zur veranschaulichung. Gesa und Sofya bitte mal draufschauen

---
+++ groundtruth_plots.py
@@ -0,0 +1,14 @@
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+filename= "ground truth/20240202_DrugCombDB_v20190531_2drug_SynDrugComb_fda_drugbank-id.tsv"
+
+groundtruth_all = pd.read_csv(filename, sep="\t")
+
+columns_to_remove = ['drugA', 'drugB']
+filtered_features = groundtruth_all.dropna()
+#insgesamt 60 Zeilen mit NA herausgefiltert
+filtered_features.to_csv("ground truth/20240202_DrugCombDB_v20190531_2drug_SynDrugComb_fda_drugbank-id_filtered.tsv", sep="\t", index=False)
